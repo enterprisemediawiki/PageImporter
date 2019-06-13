09b6870d5b3dcd640cde4a1515f3d206a24255fa
James Montalvo <edwin.j.montalvo@nasa.gov>
2019-06-13 17:10:17 -0500

    Copy dir, not symlink; Limit tests while figuring things out

--- tests/travis/install-extensions.sh
+++ tests/travis/install-extensions.sh
@@ -5,10 +5,17 @@
 set -ex
 
 BASE_PATH=$(pwd)
-MW_INSTALL_PATH=$BASE_PATH/../mw
+cd ..
+MW_INSTALL_PATH=$(pwd)/mw
+
+echo "base path:"
+ls $BASE_PATH
+
+echo "mw install path:"
+ls $MW_INSTALL_PATH
 
 cd $MW_INSTALL_PATH/extensions
-ln -s $BASE_PATH $MW_INSTALL_PATH/extensions/PageImporter
+cp -r $BASE_PATH $MW_INSTALL_PATH/extensions/PageImporter
 
 echo
 echo "require_once \"$MW_INSTALL_PATH/extensions/PageImporter/PageImporter.php\";"
