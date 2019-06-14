5642a4c8f62481fd2d421149c87e8ec7b26001a0
James Montalvo <edwin.j.montalvo@nasa.gov>
2019-06-13 22:31:42 -0500

    Check that page doesn't match when it shouldn't

--- tests/travis/run-tests.sh
+++ tests/travis/run-tests.sh
@@ -19,14 +19,23 @@ NC='\033[0m' # No Color
 fn_compare_file_with_page () {
 	PAGE_NAME="$1"
 	FILE_NAME="$2"
+	INTEND_MATCH="$3"
 
 	PAGE_VALUE=$(php $MW_INSTALL_PATH/maintenance/getText.php "$PAGE_NAME")
 	FILE_VALUE=$(cat $MW_INSTALL_PATH/extensions/ExampleExtension/ImportFiles/$FILE_NAME)
 
-	if [ "$PAGE_VALUE" = "$FILE_VALUE" ]; then
-		echo -e "$GREEN$PAGE_NAME matches $FILE_NAME$NC"
+	if [ "$INTEND_MATCH" = "no" ]; then
+		if [ "$PAGE_VALUE" = "$FILE_VALUE" ]; then
+			echo -e "$RED$PAGE_NAME matches $FILE_NAME and it should not$NC" && false
+		else
+			echo -e "$GREEN$PAGE_NAME does not match $FILE_NAME and it should not$NC"
+		fi
 	else
-		echo -e "$RED$PAGE_NAME does not match $FILE_NAME$NC" && false
+		if [ "$PAGE_VALUE" = "$FILE_VALUE" ]; then
+			echo -e "$GREEN$PAGE_NAME matches $FILE_NAME$NC"
+		else
+			echo -e "$RED$PAGE_NAME does not match $FILE_NAME$NC" && false
+		fi
 	fi
 }
 
@@ -36,6 +45,9 @@ fn_compare_file_with_page "Category:Test_Category" "Category/Test_Category.media
 # Edit pages
 echo "New template text" | php $MW_INSTALL_PATH/maintenance/edit.php "Template:Test"
 echo "New category text" | php $MW_INSTALL_PATH/maintenance/edit.php "Category:Test_Category"
+
+fn_compare_file_with_page "Template:Test" "Template/Test.mediawiki" "no"
+fn_compare_file_with_page "Category:Test_Category" "Category/Test_Category.mediawiki" "no"
 
 # Push the changes from the wiki back to the extension's files
 php importPages.php --export
